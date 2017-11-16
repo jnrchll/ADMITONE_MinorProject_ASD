3ed04460c6595574c8daa7b39d9fd39c9ef388e3
jnrchll <jeanrachelle10@outlook.com>
2017-11-16 21:17:54 +0800

    added index2.php

---
+++ index2.php
@@ -0,0 +1,28 @@
+        <div id="display">
+			<div class="fling-minislide">
+				<img src="img/thor.jpg" alt="Slide4"/>
+				<img src="img/badgenius.jpg" alt="Slide3"/>
+				<img src="img/geostorm.jpg" alt="Slide2"/>
+				<img src="img/theforeigner.jpg" alt="Slide1"/>
+			</div>
+		</div>
+		<!-- login form -->
+		<div class="register">
+				<form action="loginsave.php" method="POST">
+				<input type="hidden" name="task" value="user_validate">
+				<table>
+					<tr>
+						<td><div id="frm">Email Address: </div></td>
+						<td><input id="type" type="text" name="emailadd" value="" /></br></td>
+					</tr>
+					<tr>
+						<td><div id="frm">Password: </div></td>
+						<td><input id="type" type="password" name="password1" value="" /></br></td>
+					</tr>
+					<tr>
+					<td><input id="type" type="submit" name="submitlogin" value="LOG IN" /></td>
+					</tr>
+				</table>
+				</form>
+		</div>
+		</div>
