8f623149b21eb886e99f6280e0f8b98e38655b6c
eibieseidi <athrisha.graza02@gmail.com>
2017-11-18 20:28:06 +0800

    modified managemovies.php

--- managemovies.php
+++ managemovies.php
@@ -57,30 +57,30 @@ include("database.php");
 	<form action="moviesave.php" method="POST">
 
 					<tr>
-						<td><div id="">Movie Title: </div></td>
-						<td><input id="type" type="text" name="movietitle" value="" /></td>
+						<td><div id="in">Movie Title: </div></td>
+						<td><input id="in" type="text" name="movietitle" value="" /></td>
 					</tr>
 					<tr>
-						<td><div id="">Movie Description: </div></td>
-						<td><textarea name="description" value=""></textarea><td>
+						<td><div id="in">Movie Description: </div></td>
+						<td><textarea id="in" name="description" value=""></textarea><td>
 					</tr>
 					<tr>
-						<td><div id="">Cinema: </div></td> 
+						<td><div id="in">Cinema: </div></td> 
 						<td>
 							<select name="cinema">
-								<option value="1" selected>Cinema 1</option>
-								<option value="2">Cinema 2</option>
-								<option value="3">Cinema 3</option>	
-								<option value="4">Cinema 4</option>
+								<option id="in" value="1" selected>Cinema 1</option>
+								<option id="in" value="2">Cinema 2</option>
+								<option id="in" value="3">Cinema 3</option>	
+								<option id="in" value="4">Cinema 4</option>
 							</select>
 						</td>
 					</tr>
 					<tr>
-						<td><div id="">Movie Image Filename: </div></td>
-						<td><input id="type" type="text" name="imgfilename" value="" /></br></td>
+						<td><div id="in">Movie Image Filename: </div></td>
+						<td><input id="in" type="text" name="imgfilename" value="" /></br></td>
 					</tr>
 					<tr>
-					<td><input id="type" type="submit" name="submit" value="Add" /></td>
+					<td><input id="in" type="submit" name="submit" value="Add" /></td>
 					</tr>
 	</form>
 </div>
