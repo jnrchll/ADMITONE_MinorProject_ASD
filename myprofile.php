065fe00760fa043a57b3804e7088c45f97cd96cd
eibieseidi <athrisha.graza02@gmail.com>
2017-11-20 00:56:52 +0800

    modified myprofile.php

--- myprofile.php
+++ myprofile.php
@@ -1,10 +1,20 @@
 <?php
 session_start(); 
-
 $loggedinID = $_SESSION['userID'];
-				
+
 include("menu2.php");
+include("database.php");
+
+$query = "SELECT * FROM moviesinfo";
+	
+$result = mysqli_query($connection, $query);
 ?>
+ <div id="menu">
+	
+				<a class="mnu" href="home.php"><img src="img/roll.png" width="30" height="30">Now Showing</a>
+				<a class="mnu" href="comingsoon.php"><img src="img/film.png" width="30" height="30">Coming Soon</a>
+				<a class="mnu" href="promosevents.php"><img src="img/popcorn.png" width="30" height="30">Promos and Events</a>
+			</div>   
             
 			<div id="register">
 			<?php 
@@ -53,6 +63,6 @@ include("menu2.php");
 			</div>
 			
 		</div>
-	</div>
+	
 
 </html>
